270bf3cbe5b5f774308d9de1884419059db4097f
YunSeok-Choi <dbstjr9898@naver.com>
2022-04-28 23:31:39 +0900

    add memo

--- MemoApp/ContentView.swift
+++ MemoApp/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("메모")
+            .font(.title)
     }
 }
 
