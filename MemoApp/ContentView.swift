57350348d40471f2c6c8d0674ebc7c9b64539568
YunSeok-Choi <dbstjr9898@naver.com>
2022-05-04 21:04:03 +0900

    add space

--- MemoApp/ContentView.swift
+++ MemoApp/ContentView.swift
@@ -42,7 +42,6 @@ struct ContentView: View {
                         } label: {
                             ListView(memo: memo)
                         }
-                        
                     }
                 }.listStyle(.plain)
                     
